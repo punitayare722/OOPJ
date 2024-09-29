c66f4b8b321c4403a9ec33fad38996437362f4ef
punitayare722 <punit.235857101@vcet.edu.in>
2024-09-29 18:41:25 +0530

    Update for_loop.java

--- EX1/for_loop.java
+++ EX1/for_loop.java
@@ -1,4 +1,4 @@
-public class Main {
+public class for_loop{
     public static void main(String[] args) {
         // For loop that prints numbers from 1 to 5
         for (int i = 1; i <= 5; i++) {
